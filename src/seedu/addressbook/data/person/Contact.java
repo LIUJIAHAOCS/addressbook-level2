0ca1070e5a9e900e5216c3d6d05b407bc0242d68
LIU JIAHAO <1205965e@gmail.com>
2017-02-07 21:36:04 +0800

    Create Class Contact

---
+++ src/seedu/addressbook/data/person/Contact.java
@@ -0,0 +1,38 @@
+package seedu.addressbook.data.person;
+
+import seedu.addressbook.data.exception.IllegalValueException;
+
+public class Contact {
+    public final String value;
+    private boolean isPrivate;
+    
+    public Contact(String value, boolean isPrivate, String messageContactConstraints, String contactValidationRegex) throws IllegalValueException {
+        this.isPrivate = isPrivate;
+        String trimmedContact = value.trim();
+        if (!isValidContact(trimmedContact,contactValidationRegex)) {
+            throw new IllegalValueException(messageContactConstraints);
+        }
+        this.value = trimmedContact;
+    }
+    
+    /**
+     * Checks if a given string is a valid Contact.
+     */
+    public static boolean isValidContact(String test, String contactValidationRegex) {
+        return test.matches(contactValidationRegex);
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+
+    public boolean isPrivate() {
+        return isPrivate;
+    }
+}
